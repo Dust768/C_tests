530129a1212b8176a0259dd384c0573ff47f80ec
fluquan <fluquan@gmail.com>
2023-01-24 16:35:51 +0800

    Structures example

---
+++ C_test_2/C_test_2/C_text_2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+struct student
+{
+	char name[20];
+	int age;
+	char gender;
+};
+
+int main()
+{
+	struct student Ned = { "Ned", 14,'m' };
+	printf("%s %d %c\n", Ned.name, Ned.age, Ned.gender);
+	struct student* PNed = &Ned;
+	printf("%s %d %c\n", (*PNed).name, (*PNed).age, (*PNed).gender);
+	printf("%s %d %c\n", PNed->name, PNed->age, PNed->gender);
+	return 0;
+}
